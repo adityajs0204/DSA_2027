382bc79e8f2effd2207f25d5c19a8041d125d5e3
adityajs0204 <adityajs2006@gmail.com>
2025-08-17 22:45:19 +0530

    Update Swap_Nodes.cpp

--- Linked List/Swap_Nodes.cpp
+++ Linked List/Swap_Nodes.cpp
@@ -4,15 +4,35 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {} 
 };
 
 ListNode* swapPairs(ListNode* head) {
     if (head == nullptr) return nullptr;
     if (head->next == nullptr) return head;
-    
     ListNode* p = head;
     ListNode* q = head->next;
     p->next = swapPairs(q->next);
     q->next = p;
     return q;
+}
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    cout << "Original list: ";
+    printList(head);
+    head = swapPairs(head);
+    cout << "After swapping pairs: ";
+    printList(head);
+
+    return 0;
 }
